f4f7a54897dfa5cc57b15e615f6458c7dd127280
Ah1To <Ah1to@yandex.ru>
2018-12-19 13:23:19 +0500

    Save Project alpha 0.0.1

---
+++ springMVC/src/main/java/net/ah1to/sweater/repos/MessageRepo.java
@@ -0,0 +1,10 @@
+package net.ah1to.sweater.repos;
+
+import net.ah1to.sweater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}
---
+++ springMVC/src/main/java/net/ah1to/sweater/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package net.ah1to.sweater.repos;
+
+import net.ah1to.sweater.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
